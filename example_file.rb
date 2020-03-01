c5a2281a5d8f7209cde7d1645104a2447255d82f
spectacular-system-6224 <d.leslie707@gmail.com>
2020-03-01 12:29:03 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,2 @@
+print "hello world"
+p "hello"
---
+++ greeting.rb
@@ -0,0 +1,16 @@
+def greeting
+  puts "Hello World Dada"
+end
+
+def say_greeting_five_time
+    greeting
+    greeting
+    greeting
+    greeting
+    greeting
+    
+end
+
+say_greeting_five_time
+say_greeting_five_time
+
